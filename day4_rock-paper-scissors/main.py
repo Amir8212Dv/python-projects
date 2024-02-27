e01234f54c82e6d268ce4bf8464b804122d1b005
Amir Mohammad Razmi <88226929+Amir8212Dv@users.noreply.github.com>
2024-02-27 19:54:46 +0330

    update rock-paper-scissors game

--- day4_rock-paper-scissors/main.py
+++ day4_rock-paper-scissors/main.py
@@ -1,9 +1,12 @@
 import signs
 import random
 
-options = [["Rock" , signs.rock] , ["Paper" , signs.paper], ["Scissors" , signs.scissors]]
+options = [["rock" , signs.rock] , ["paper" , signs.paper], ["scissors" , signs.scissors]]
 
 user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+if user_input > 3 or user_input < 0:
+  print("Try again and enter a valid number (0 , 1 , 2)")
+  quit()
 
 user_choice = options[user_input]
 computer_choice = options[random.randint(0,2)]
@@ -19,20 +22,20 @@ draw_message = "Draw"
 
 if user_choice[0] == computer_choice[0]:
   outcome = draw_message
-elif user_choice[0] == "Rock":
-  if computer_choice[0] == "Paper":
+elif user_choice[0] == "rock":
+  if computer_choice[0] == "paper":
     outcome = lose_message
-  else: #Scissors
+  else: #scissors
     outcome = win_message
-elif user_choice[0] == "Paper":
-  if computer_choice[0] == "Rock":
+elif user_choice[0] == "paper":
+  if computer_choice[0] == "rock":
     outcome = win_message
-  else: #Scissors
+  else: #scissors
     outcome = lose_message
-elif user_choice[0] == "Scissors":
-  if computer_choice[0] == "Rock":
+elif user_choice[0] == "scissors":
+  if computer_choice[0] == "rock":
     outcome = lose_message
-  else : #Paper
+  else : #paper
     outcome = win_message
 
 print(outcome)
