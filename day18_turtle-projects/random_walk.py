da296e4eefc8f00ac15379b065aed3beac10690b
Amir Mohammad Razmi <88226929+Amir8212Dv@users.noreply.github.com>
2024-03-18 17:14:21 +0330

    feat: update random walk project

--- day18_turtle-projects/random_walk.py
+++ day18_turtle-projects/random_walk.py
@@ -1,22 +1,20 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, colormode
 import random
 
 
 the_turtle = Turtle("arrow")
 the_turtle.speed("fastest")
 the_turtle.pensize(15)
+colormode(255)
 
 screen = Screen()
 
-colors = [
-    "blue",
-    "green",
-    "red",
-    "yellow",
-    "orange",
-    "brown",
-    "black",
-]
+def generate_random_rgb():
+    r = random.randint(0 , 255)
+    g = random.randint(0 , 255)
+    b = random.randint(0 , 255)
+    return r,g,b
+
 angles = [0, 90, 180, 270]
 
 
@@ -26,7 +24,7 @@ while True:
     if abs(pos_x) > max_x + 40 or abs(pos_y) > max_y + 40:
         break
 
-    the_turtle.color(random.choice(colors))
+    the_turtle.pencolor(generate_random_rgb())
     the_turtle.right(random.choice(angles))
     the_turtle.forward(40)
 
