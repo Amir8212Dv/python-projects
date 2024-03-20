caaa9a18156c2ece4fd606abcd92babc1ccecffc
Amir Mohammad Razmi <88226929+Amir8212Dv@users.noreply.github.com>
2024-03-20 18:53:59 +0330

    use class inheritance in score class

--- day20_day21_snake-game/score.py
+++ day20_day21_snake-game/score.py
@@ -2,15 +2,15 @@ from turtle import Turtle
 from constants import SCREEN_HEIGHT
 
 
-class Score:
+class Score(Turtle):
     def __init__(self):
-        score_turtle = Turtle()
-        score_turtle.hideturtle()
-        score_turtle.pu()
-        score_turtle.goto(-30, SCREEN_HEIGHT / 2 - 50)
-        score_turtle.color("white")
-        self.score_turtle = score_turtle
+        super().__init__()
+
         self.score = 0
+        self.pu()
+        self.hideturtle()
+        self.goto(-30, SCREEN_HEIGHT / 2 - 50)
+        self.color("white")
         self.update_board()
 
     def gain(self):
@@ -18,5 +18,5 @@ class Score:
         self.update_board()
 
     def update_board(self):
-        self.score_turtle.clear()
-        self.score_turtle.write(f"Score : {self.score}", False, "left", ("Arial", 15, "normal"))
+        self.clear()
+        self.write(f"Score : {self.score}", False, "left", ("Arial", 15, "normal"))
