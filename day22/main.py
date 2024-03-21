678b613b0297ca070d9ff8b0d80471215b37aef5
Amir Mohammad Razmi <88226929+Amir8212Dv@users.noreply.github.com>
2024-03-21 21:48:30 +0330

    update paddle class

--- day22/main.py
+++ day22/main.py
@@ -1,10 +1,11 @@
 from screen import GameScreen
 from paddle import Paddle
 from ball import Ball
+from constants import SCREEN_WIDTH
 
 screen = GameScreen()
-left_paddle = Paddle("left")
-right_paddle = Paddle("right")
+left_paddle = Paddle((-SCREEN_WIDTH / 2 + 20, 0))
+right_paddle = Paddle((SCREEN_WIDTH / 2 - 20, 0))
 
 screen.set_paddles(left_paddle, right_paddle)
 ball = Ball(left_paddle, right_paddle, screen.refresh, screen.switch_active_paddle)
--- day22/paddle.py
+++ day22/paddle.py
@@ -1,24 +1,22 @@
 from turtle import Turtle
-from constants import SCREEN_HEIGHT,SCREEN_WIDTH
+from constants import SCREEN_HEIGHT
+
 
 class Paddle(Turtle):
-    def __init__(self, side: str):
+    def __init__(self, position: tuple):
         super().__init__()
         self.color("white")
         self.shape("square")
-        self.shapesize(4,0.5,5)
+        self.shapesize(4, 0.5, 5)
         self.pu()
-        if side == "left":
-            self.goto(-SCREEN_WIDTH / 2 + 20, 0)
-        elif side == "right":
-            self.goto(SCREEN_WIDTH / 2 - 20, 0)
+        self.goto(position)
 
     def move_up(self):
-        x,y = self.pos()
+        x, y = self.pos()
         if SCREEN_HEIGHT / 2 - 50 > y:
             self.goto(x, y + 40)
 
     def move_down(self):
-        x,y = self.pos()
+        x, y = self.pos()
         if SCREEN_HEIGHT / 2 - 60 > -y:
             self.goto(x, y - 40)
