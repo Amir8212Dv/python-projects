88efdac4d7e95fe5fbb41bd3339704a6975a48b0
Amir Mohammad Razmi <88226929+Amir8212Dv@users.noreply.github.com>
2024-03-22 18:09:27 +0330

    add high-score feature to  snake game

--- day20_21_snake-game/board.py
+++ day20_21_snake-game/board.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
-from constants import SCREEN_HEIGHT
+from constants import SCREEN_HEIGHT, HIGH_SCORE_FILE_PATH
+import os
 
 
 class Board(Turtle):
@@ -8,8 +9,16 @@ class Board(Turtle):
 
         self.score = 0
         self.pu()
+
+        self.high_score = 0
+        if os.path.exists(HIGH_SCORE_FILE_PATH):
+            with open(HIGH_SCORE_FILE_PATH) as high_score_file:
+                file_content = high_score_file.read()
+                if file_content:
+                    self.high_score = int(file_content)
+
         self.hideturtle()
-        self.goto(-30, SCREEN_HEIGHT / 2 - 50)
+        self.goto(-70, SCREEN_HEIGHT / 2 - 50)
         self.color("white")
         self.print_score()
 
@@ -19,8 +28,11 @@ class Board(Turtle):
 
     def print_score(self):
         self.clear()
-        self.write(f"Score : {self.score}", False, "left", ("Arial", 15, "normal"))
+        self.write(f"Score : {self.score}  High Score: {self.high_score}", False, "left", ("Arial", 15, "normal"))
 
     def game_over(self):
         self.goto(0, 0)
         self.write(f"Game Over", False, "center", ("Arial", 15, "normal"))
+        if self.score > self.high_score:
+            with open(HIGH_SCORE_FILE_PATH, "w") as high_score_file:
+                high_score_file.write(str(self.score))
--- day20_21_snake-game/constants.py
+++ day20_21_snake-game/constants.py
@@ -1,3 +1,4 @@
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 800
 MOVE_STEP_SIZE = 20
+HIGH_SCORE_FILE_PATH = "./day20_21_snake-game/high_score.txt"
