2b81778a29a0a8bde45b88641c551b50a584bbf4
Amir Mohammad Razmi <88226929+Amir8212Dv@users.noreply.github.com>
2024-03-23 18:13:19 +0330

    update state quiz

--- day25_states-quiz/main.py
+++ day25_states-quiz/main.py
@@ -9,20 +9,30 @@ screen = turtle.Screen()
 screen.bgpic("blank_states_img.gif")
 screen.title("U.S. States game")
 screen.listen()
-screen.onkey(fun=screen.bye, key="Escape")
 
-available_states_list = data.state.tolist()
-all_states_count = len(available_states_list)
+missing_states = data.state.tolist()
+all_states_count = len(missing_states)
 
-while len(available_states_list) != 0:
+
+def exit_quiz():
+    missing_states_data = data[data.state.isin(missing_states)]
+    missing_states_data.to_csv("missing_states.csv")
+    missing_states.clear()
+    screen.bye()
+
+
+while len(missing_states) != 0:
     user_input = screen.textinput(
-        title=f"{all_states_count - len(available_states_list)}/{all_states_count} States Correct",
+        title=f"{all_states_count - len(missing_states)}/{all_states_count} States Correct",
         prompt="What's another state name?",
     ).title()
-    if user_input in available_states_list:
+    if user_input == "Exit":
+        exit_quiz()
+
+    if user_input in missing_states:
         state_data = data[data.state == user_input]
         states_turtle.add_state_name(user_input, (int(state_data.x), int(state_data.y)))
-        available_states_list.remove(user_input)
+        missing_states.remove(user_input)
 
 
 screen.exitonclick()
