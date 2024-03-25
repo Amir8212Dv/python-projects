dfd75588ccfad7223ebbd0430f8becf01e5af892
Amir Mohammad Razmi <88226929+Amir8212Dv@users.noreply.github.com>
2024-03-25 20:37:05 +0330

    update names in day27 project

--- day27_converter/main.py
+++ day27_converter/main.py
@@ -6,27 +6,27 @@ window = Tk()
 window.title("Mile To Km Converter")
 window.config(padx=30, pady=15)
 
-input = Entry(width=15)
-input.grid(column=2, row=1)
+miles_input = Entry(width=15)
+miles_input.grid(column=2, row=1)
 
 miles_label = Label(text="Miles", pady=10, font=font)
 miles_label.grid(column=3, row=1)
 
 
-desc_1 = Label(text="is equal to", font=font)
-desc_1.grid(column=1, row=2)
+is_equal_label = Label(text="is equal to", font=font)
+is_equal_label.grid(column=1, row=2)
 
-converted_value_label = Label(text="0", padx=50, font=font)
-converted_value_label.grid(column=2, row=2)
+km_value_label = Label(text="0", padx=50, font=font)
+km_value_label.grid(column=2, row=2)
 
-measure_name = Label(text="KM", font=font)
-measure_name.grid(column=3, row=2)
+km_label = Label(text="KM", font=font)
+km_label.grid(column=3, row=2)
 
 
 def calculate():
-    mile = int(input.get() or "0")
+    mile = int(miles_input.get() or "0")
     km = mile * 1.609
-    converted_value_label.config(text=f"{km}")
+    km_value_label.config(text=f"{km}")
 
 
 calculate_button = Button(
