fb82d708ae9137bb2f25448392011ef9abc18d91
Amir Mohammad Razmi <88226929+Amir8212Dv@users.noreply.github.com>
2024-03-26 20:49:35 +0330

    add reset button functionality

--- day28_pomodoro-app/main.py
+++ day28_pomodoro-app/main.py
@@ -22,24 +22,37 @@ canvas.create_image(102, 112, image=tomato_img)
 timer = canvas.create_text(100, 112, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
 canvas.place(x=250, y=200)
 
+remain_time = 0
+
 
 def start():
-    global timer
-    for i in range(60 * 25, 0, -1):
-        m = str(i // 60)
+    global timer, remain_time
+    remain_time = 60 * 25
+
+    while remain_time > 0:
+        m = str(remain_time // 60)
         m = f"0{m}" if len(m) == 1 else m
-        s = str(i % 60)
+        s = str(remain_time % 60)
         s = f"0{s}" if len(s) == 1 else s
         canvas.delete(timer)
         timer = canvas.create_text(100, 112, text=f"{m}:{s}", fill="white", font=(FONT_NAME, 25, "bold"))
         w.update()
         time.sleep(1)
+        remain_time -= 1
+
+
+def reset():
+    global timer, remain_time
+    remain_time = 0
+    canvas.delete(timer)
+    timer = canvas.create_text(100, 112, text=f"00:00", fill="white", font=(FONT_NAME, 25, "bold"))
+    w.update()
 
 
 start_button = tkinter.Button(text="start", command=start)
 start_button.place(x=150, y=500)
 
-reset_button = tkinter.Button(text="reset")
+reset_button = tkinter.Button(text="reset", command=reset)
 reset_button.place(x=500, y=500)
 
 
