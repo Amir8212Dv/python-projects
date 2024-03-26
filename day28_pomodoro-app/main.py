054561ca8295fbb934949ebb11a2aa69f2fb5f47
Amir Mohammad Razmi <88226929+Amir8212Dv@users.noreply.github.com>
2024-03-26 21:04:27 +0330

    complete pomodoro project

--- day28_pomodoro-app/main.py
+++ day28_pomodoro-app/main.py
@@ -13,8 +13,8 @@ w.config(bg=YELLOW)
 w.minsize(width=700, height=700)
 w.title("Pomodoro")
 
-timer_label = tkinter.Label(text="Timer", font=(FONT_NAME, 60))
-timer_label.place(x=250, y=50)
+status_label = tkinter.Label(text="Timer", font=(FONT_NAME, 60), bg=YELLOW, fg=GREEN)
+status_label.place(x=250, y=50)
 
 canvas = tkinter.Canvas(width=202, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = tkinter.PhotoImage(file="tomato.png")
@@ -22,28 +22,48 @@ canvas.create_image(102, 112, image=tomato_img)
 timer = canvas.create_text(100, 112, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
 canvas.place(x=250, y=200)
 
-remain_time = 0
+work_time = 0
+break_time = 0
 
 
 def start():
-    global timer, remain_time
-    remain_time = 60 * 25
+    global timer, work_time, break_time
+    work_time = 60 * 25 * 0.01
+    break_time = 60 * 5 * 0.01
 
-    while remain_time > 0:
-        m = str(remain_time // 60)
+    status_label.config(text="Work")
+    while work_time > 0:
+        m = str(int(work_time // 60))
         m = f"0{m}" if len(m) == 1 else m
-        s = str(remain_time % 60)
+        s = str(int(work_time % 60))
         s = f"0{s}" if len(s) == 1 else s
         canvas.delete(timer)
         timer = canvas.create_text(100, 112, text=f"{m}:{s}", fill="white", font=(FONT_NAME, 25, "bold"))
         w.update()
         time.sleep(1)
-        remain_time -= 1
+        work_time -= 1
+
+    if break_time:
+        status_label.config(text="Break")
+    while break_time >= 0:
+        m = str(int(break_time // 60))
+        m = f"0{m}" if len(m) == 1 else m
+        s = str(int(break_time % 60))
+        s = f"0{s}" if len(s) == 1 else s
+        canvas.delete(timer)
+        timer = canvas.create_text(100, 112, text=f"{m}:{s}", fill="white", font=(FONT_NAME, 25, "bold"))
+        w.update()
+        time.sleep(1)
+        break_time -= 1
+        if break_time == 0:
+            start()
 
 
 def reset():
-    global timer, remain_time
-    remain_time = 0
+    global timer, work_time, break_time
+    work_time = 0
+    break_time = 0
+    status_label.config(text="Timer")
     canvas.delete(timer)
     timer = canvas.create_text(100, 112, text=f"00:00", fill="white", font=(FONT_NAME, 25, "bold"))
     w.update()
