6be78823a55f24df2566cf363c87c15bb2fc85c1
Amir Mohammad Razmi <88226929+Amir8212Dv@users.noreply.github.com>
2024-03-28 18:52:36 +0330

    refactor pomodoro app

--- day28_pomodoro-app/main.py
+++ day28_pomodoro-app/main.py
@@ -1,4 +1,4 @@
-import tkinter
+from tkinter import Label, Button, Canvas, Tk, PhotoImage
 import time
 
 YELLOW = "#f7f5dd"
@@ -8,24 +8,22 @@ PINK = "#e2979c"
 FONT_NAME = "Courier"
 CHECK_MARK = "✔️"
 
-w = tkinter.Tk()
+w = Tk()
 w.config(bg=YELLOW)
 w.minsize(width=700, height=700)
 w.title("Pomodoro")
 
-status_label = tkinter.Label(text="Timer", font=(FONT_NAME, 60), bg=YELLOW, fg=GREEN)
+status_label = Label(text="Timer", font=(FONT_NAME, 60), bg=YELLOW, fg=GREEN)
 status_label.place(x=250, y=50)
 
-canvas = tkinter.Canvas(width=202, height=224, bg=YELLOW, highlightthickness=0)
-tomato_img = tkinter.PhotoImage(file="tomato.png")
+canvas = Canvas(width=202, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(102, 112, image=tomato_img)
 timer = canvas.create_text(100, 112, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
 canvas.place(x=250, y=200)
 
 
-complete_work_sessions_label = tkinter.Label(
-    text="Completed Sessions: 0", fg=GREEN, bg=YELLOW, font=("Arial", 10, "bold")
-)
+complete_work_sessions_label = Label(text="Completed Sessions: 0", fg=GREEN, bg=YELLOW, font=("Arial", 10, "bold"))
 complete_work_sessions_label.place(x=275, y=500)
 
 work_time = 0
@@ -33,33 +31,33 @@ break_time = 0
 completed_work_sessions = 0
 
 
+def update_timer(time: int):
+    global timer
+
+    m = str(int(time // 60))
+    m = f"0{m}" if len(m) == 1 else m
+    s = str(int(time % 60))
+    s = f"0{s}" if len(s) == 1 else s
+    canvas.delete(timer)
+    timer = canvas.create_text(100, 112, text=f"{m}:{s}", fill="white", font=(FONT_NAME, 25, "bold"))
+    w.update()
+
+
 def start():
-    global timer, work_time, break_time, completed_work_sessions
-    work_time = 60 * 25 * 0.01
+    global work_time, break_time, completed_work_sessions
+    work_time = 60 * 25 * 0.001
     break_time = 60 * 5 * 0.01
 
     status_label.config(text="Work")
     while work_time > 0:
-        m = str(int(work_time // 60))
-        m = f"0{m}" if len(m) == 1 else m
-        s = str(int(work_time % 60))
-        s = f"0{s}" if len(s) == 1 else s
-        canvas.delete(timer)
-        timer = canvas.create_text(100, 112, text=f"{m}:{s}", fill="white", font=(FONT_NAME, 25, "bold"))
-        w.update()
+        update_timer(work_time)
         time.sleep(1)
         work_time -= 1
 
     if break_time:
         status_label.config(text="Break")
     while break_time >= 0:
-        m = str(int(break_time // 60))
-        m = f"0{m}" if len(m) == 1 else m
-        s = str(int(break_time % 60))
-        s = f"0{s}" if len(s) == 1 else s
-        canvas.delete(timer)
-        timer = canvas.create_text(100, 112, text=f"{m}:{s}", fill="white", font=(FONT_NAME, 25, "bold"))
-        w.update()
+        update_timer(break_time)
         time.sleep(1)
         break_time -= 1
         if break_time == 0:
@@ -78,10 +76,10 @@ def reset():
     w.update()
 
 
-start_button = tkinter.Button(text="start", command=start, bg="white", border=0, width=10, font=("Arial", 12))
+start_button = Button(text="start", command=start, bg="white", border=0, width=10, font=("Arial", 12))
 start_button.place(x=150, y=500)
 
-reset_button = tkinter.Button(text="reset", command=reset, bg="white", border=0, width=10, font=("Arial", 12))
+reset_button = Button(text="reset", command=reset, bg="white", border=0, width=10, font=("Arial", 12))
 reset_button.place(x=450, y=500)
 
 
